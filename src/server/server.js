2c06eacb41976c01b7a08bfd70c4cfbc380693ee
kevlampiev <kevlampiev@gmail.com>
2019-11-08 01:23:19 +0700

    greetings 2

--- src/server/server.js
+++ src/server/server.js
@@ -1,8 +1,9 @@
-let http = require('http');
+const http = require('http');
+const greeting = require('./greeting');
 
 http.createServer((res, req) => {
     //    res.writeHead(200);
-    req.end('Hello from server....');
+    req.end(greeting.getMessage() + '.....');
 }).listen(8081, "127.0.0.1", () => {
     console.log('Listening at 8081...');
 });
